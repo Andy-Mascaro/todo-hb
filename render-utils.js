a22990deba40463bd1ceba6f9473f7b6049e073b
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-02-16 18:48:52 -0800

    worked on render utils

--- render-utils.js
+++ render-utils.js
@@ -1,10 +1,13 @@
 export function renderTodo(todo) {
-    const li = document.createElement('li');
-    if (todo.complete) {
-        li.classList.add('complete');
-    }
-    li.textContent = todo.description;
-    return li;
+    const p = document.createElement('p');
+    const div = document.createElement('div');
+    div.classList.add(todo.complete ? 'complete' : 'incomplete'); 
+    div.classList.add('todo');
+    
+    p.textContent = todo.todo;
+
+    div.append(p);
+    return div;
 }
     // create a div and a p tag
     // depending on whether the todo is complete, give the div the appropriate css class ('complete' or 'incomplete')
